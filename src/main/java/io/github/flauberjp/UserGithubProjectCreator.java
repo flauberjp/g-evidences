8320ea94a28298d29ddad26cae4fdd6b2e94bbe5
Flaviano Flauber <flauberjp@gmail.com>
2020-05-26 23:22:13 -0300

    Informações do github sendo lidas em arquivo externo (um resource por hora)

--- src/main/java/io/github/flauberjp/UserGithubProjectCreator.java
+++ src/main/java/io/github/flauberjp/UserGithubProjectCreator.java
@@ -1,17 +1,20 @@
 package io.github.flauberjp;
 
+import java.util.Properties;
 import org.kohsuke.github.GHCreateRepositoryBuilder;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class UserGithubProjectCreator {
     public static void main(String[] args) throws IOException, URISyntaxException {
-        GitHub github = new GitHubBuilder().withPassword("flauberjp", "xxxxxx").build();
+        Properties properties = new Properties();
+        properties.load(UserGithubProjectCreator.class.getResourceAsStream(".github"));
+        GitHub github = GitHubBuilder
+                .fromProperties(properties)
+                .build();
 
         GHCreateRepositoryBuilder repo = github.createRepository("new-repository");
         repo.description("description-of-new-repository");
