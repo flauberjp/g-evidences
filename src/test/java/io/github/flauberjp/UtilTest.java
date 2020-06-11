966632f511d5465a187da23504b8cc2db5d4a467
Flaviano Flauber <flauberjp@gmail.com>
2020-06-10 22:51:18 -0300

    refactoring: removendo código não necessário da parte dos testes

--- src/test/java/io/github/flauberjp/UtilTest.java
+++ src/test/java/io/github/flauberjp/UtilTest.java
@@ -2,20 +2,9 @@ package io.github.flauberjp;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import com.google.common.jimfs.Configuration;
-import com.google.common.jimfs.Jimfs;
-import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.channels.Channels;
-import java.nio.channels.WritableByteChannel;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.util.Random;
 import org.junit.jupiter.api.Test;
 
@@ -23,20 +12,7 @@ class UtilTest {
 
   @Test
   void convertIndexHtmlResourceToFileTest() throws IOException {
-    InputStream resourceAsStream = UserGithubProjectCreator.class
-        .getResourceAsStream("initialProjectTemplate/template_index.html");
-    byte[] buffer = new byte[resourceAsStream.available()];
-    resourceAsStream.read(buffer);
-
-    String tmpdir = "/tmp";
-    FileSystem fs = Jimfs.newFileSystem(Configuration.unix());
-    Path tmp = fs.getPath(tmpdir);
-    Files.createDirectory(tmp);
-
-    Path hello = tmp.resolve("index.html"); // /tmp/index.html
-    Files.write(hello, buffer, StandardOpenOption.CREATE);
-    assertTrue(Files.exists(hello));
-    assertTrue(Files.readString(hello).contains("<html>"));
+    convertResourceToFileTest("template_index.html");
   }
 
   @Test
