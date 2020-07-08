1c26b2c3a3944386f693d30cd8c64a32d6a8fed1
Flaviano Flauber <flauberjp@gmail.com>
2020-07-08 17:59:38 -0300

    #62: Bug: a versão esta sendo pega do pom.xml, qdo o programa é executado a partir de um jar, ele não funciona

--- src/main/java/io/github/flauberjp/Version.java
+++ src/main/java/io/github/flauberjp/Version.java
@@ -2,6 +2,7 @@ package io.github.flauberjp;
 
 import static io.github.flauberjp.util.MyLogger.LOGGER;
 
+import io.github.flauberjp.util.Util;
 import java.io.FileReader;
 import java.io.IOException;
 import org.apache.maven.model.Model;
@@ -12,19 +13,22 @@ public class Version {
 
   public static void main(String[] args)
       throws IOException, XmlPullParserException {
-    MavenXpp3Reader reader = new MavenXpp3Reader();
     LOGGER.info(Version.getVersionFromPom());
   }
 
   public static String getVersionFromPom() {
     LOGGER.debug("Version.getVersionFromPom()");
-    String result = "";
-    try {
-      MavenXpp3Reader reader = new MavenXpp3Reader();
-      Model model = reader.read(new FileReader("pom.xml"));
-      result = "Version: " + model.getVersion();
-    } catch (IOException | XmlPullParserException ex) {
-      LOGGER.error(ex.getMessage(), ex);
+    String result = "Version: ";
+    if(Util.isRunningFromJar()) {
+      result += Version.class.getPackage().getImplementationVersion();
+    } else {
+      try {
+        MavenXpp3Reader reader = new MavenXpp3Reader();
+        Model model = reader.read(new FileReader("pom.xml"));
+        result += model.getVersion();
+      } catch (IOException | XmlPullParserException ex) {
+        LOGGER.error(ex.getMessage(), ex);
+      }
     }
     return result;
   }
